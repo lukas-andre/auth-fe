117dae449fbed617af9497f100ecf15ec188c474
lukas-andre <lucashenry@workmate.cl>
2020-01-08 16:17:20 -0300

    add init architecture of the project

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,22 @@
+import { Routes, RouterModule } from "@angular/router";
+import { AuthComponent } from './auth.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+
+const authRoutes: Routes = [
+    {
+        path: '',
+        component: AuthComponent,
+        children: [
+            { path: '', redirectTo: 'login', pathMatch: 'full' },
+            { path: 'login', component: LoginComponent },
+            { path: 'register', component: RegisterComponent },
+            { path: 'reset-password', component: ResetPasswordComponent },
+            { path: 'forgot-password', component: ForgotPasswordComponent },
+        ]
+    }
+];
+
+export const AUTH_ROUTES = RouterModule.forChild(authRoutes);
