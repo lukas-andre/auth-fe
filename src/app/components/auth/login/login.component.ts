62deaeca0a8219a9ed131d922fa913b96f480c23
lukas-andre <lucashenry@workmate.cl>
2020-01-09 23:54:38 -0300

    add: login component with form

---
+++ src/app/components/auth/login/login.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/store/user/user.model';
+import { select, Store } from '@ngrx/store';
+import { AppState } from 'src/app/store/app.states';
+import { LogIn } from 'src/app/store/user/user.actions';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  user: User;
+  form: FormGroup;
+
+  constructor(
+    private store: Store<AppState>,
+    private formBuilder: FormBuilder
+    ) { }
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.form =  this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+  onSubmit() {
+    const payload = {
+      username: this.user.username,
+      password: this.user.password
+    };
+    this.store.dispatch(new LogIn(payload));
+  }
+}
